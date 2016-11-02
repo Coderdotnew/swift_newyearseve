745730b6fa88009b67b2114b7ba4665be27f8d27
Stephanholly <sholly@usc.edu>
2016-11-02 10:59:34 -0700

    first commit

---
+++ nye.swift
@@ -0,0 +1,20 @@
+// New Years Eve
+//Use a loop to print out a countdown to your Terminal, counting down from 10 seconds to 1 second. Once the countdown reaches 1, your program should return `"HAPPY NEW YEAR!"`.  
+
+//Your should see the following output:  
+```swift
+
+// 10 
+// 9 
+// 8 
+// 7 
+// 6 
+// 5 
+// 4 
+// 3 
+// 2 
+// 1 
+//=> "HAPPY NEW YEAR!" # this is your return value
+```  
+
+// enter code here:
